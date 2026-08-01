1f8def1b8803ac17cb06f1c349621a708fd1ff4a
youmbaihamza9-lab <youmbaihamza9@gmail.com>
2026-08-01 01:17:16 -1100

    Update layout.js

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -2,9 +2,9 @@ import Sidebar from '@/components/Sidebar';
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="min-h-screen flex flex-row-reverse">
+    <div className="min-h-screen flex flex-col lg:flex-row-reverse">
       <Sidebar mode="admin" />
-      <main className="flex-1 min-w-0 p-6 lg:p-8">{children}</main>
+      <main className="flex-1 min-w-0 p-4 lg:p-8">{children}</main>
     </div>
   );
 }
